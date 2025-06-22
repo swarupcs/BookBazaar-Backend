a8180e65dca9a26bf65bfc1db67b950b17fe27a2
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-06-22 22:14:00 +0530

    Refactor cookie management functions to ensure proper clearing of authentication cookies

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -13,3 +13,19 @@ export const setAuthCookies = (res, accessToken, refreshToken) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
+
+
+export const clearAuthCookies = (res) => {
+    res.clearCookie("accessToken", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+    });
+  
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+    });
+  };
+  
