bb764d5aa54ac041baac8faf604c73daad3496a7
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-06-22 20:56:40 +0530

    Implement user authentication middleware and add getUserDetails endpoint for fetching user profile

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import { ApiError } from "../utils/api-error.js";
+import jwt from 'jsonwebtoken';
+import { ACCESS_TOKEN_SECRET } from "../config/serverConfig.js";
+import { User } from "../models/user.model.js";
+
+
+export const requireAuth = async (req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken;
+
+        if(!token) {
+            throw new ApiError(401, 'Unauthorized: No access token provided');
+        }
+
+        const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
+
+        const user = await User.findById(decoded._id).select('-password -refreshToken');
+
+        if(!user) {
+            throw new ApiError(401, 'Invalid token or user not found');
+        }
+
+        req.user = user;
+        next();
+
+
+    } catch (error) {
+        return next(new ApiError(401, 'Unauthorized or expiry token'));
+    }
+}
