79d1abea1c77ee5bc6ae6a4ba0a1f4bb81bd1c5b
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-06-29 09:21:11 +0530

    Add review routes to book API: implement endpoints for adding and retrieving reviews

--- src/routes/v1/book.routes.js
+++ src/routes/v1/book.routes.js
@@ -3,6 +3,7 @@ import { requireAuth } from "../../middlewares/auth.middleware.js";
 import { requireRole } from "../../middlewares/requireRole.middleware.js";
 import { createBook, deleteBook, getAllBooks, getBookById, updateBook } from "../../controllers/book.controller.js";
 import { verifyApiKey } from "../../middlewares/verifyApiKey.middleware.js";
+import { addReviewToBook, getBookReviews } from "../../controllers/review.controller.js";
 
 const bookRouter = express.Router();
 
@@ -13,5 +14,7 @@ bookRouter.get('/', getAllBooks); // public
 bookRouter.get('/:id', getBookById); // public
 bookRouter.put('/:id', requireAuth, requireRole('admin'), updateBook);
 bookRouter.delete('/:id', requireAuth, requireRole('admin'), deleteBook);
+bookRouter.post('/:bookId/reviews', requireAuth, addReviewToBook); 
+bookRouter.get('/:bookId/reviews', requireAuth, getBookReviews); 
 
 export default bookRouter;
