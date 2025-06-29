46572aebd7d1cbc1030686c0ba888e873e1fd022
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-06-29 09:21:18 +0530

    Add review routes to v1Router: include reviewRouter for handling review-related requests

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRouter from './auth.routes.js';
 import bookRouter from './book.routes.js';
+import reviewRouter from './review.routes.js';
 
 
 const v1Router = express.Router();
@@ -8,5 +9,6 @@ const v1Router = express.Router();
 
 v1Router.use("/auth", authRouter);
 v1Router.use("/books", bookRouter);
+v1Router.use("/reviews", reviewRouter);
 
 export default v1Router;
