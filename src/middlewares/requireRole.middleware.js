c1355e37c4b3eb28c4245352c750ea9bbfad88b8
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-07-05 19:33:23 +0530

    Add import statement for ApiError in requireRole middleware

--- src/middlewares/requireRole.middleware.js
+++ src/middlewares/requireRole.middleware.js
@@ -1,3 +1,5 @@
+import { ApiError } from "../utils/api-error.js";
+
 export const requireRole = (role) => (req, res, next) => {
   if (req.user?.role !== role) {
     return next(new ApiError(403, 'Access denied'));
